48ebe62cb455e771e3d091874c5bd94bab7d990f
KristPS <126523107+KristPS@users.noreply.github.com>
2023-10-10 04:07:22 -0400

    Update allCategories.jsx

--- src/Pages/Products/allCategories.jsx
+++ src/Pages/Products/allCategories.jsx
@@ -1,37 +1,74 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE_URL = 'https://fakestoreapi.com/products';
+const BASE_URL = 'https://fakestoreapi.com/products';
+
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    throw error;
+  }
+};
+
+const ProductList = ({ products }) => (
+  <ul>
+    {products.map((product) => (
+      <li key={product.id}>{product.title}</li>
+    ))}
+  </ul>
+);
 
 const AllProductCategories = () => {
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-
-  // Function to fetch data from the API
-  const fetchData = async (url) => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
-    }
-  };
-
-  // Fetch all products and extract categories
-  const getAllCategories = async () => {
-    const data = await fetchData(API_BASE_URL);
-    setProducts(data);
-
-    // Extract unique categories from products
-    const uniqueCategories = [...new Set(data.map(product => product.category))];
-    setCategories(uniqueCategories);
-  };
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Example usage of functions
-    getAllCategories();
+    const fetchDataAndCategories = async () => {
+      try {
+        const data = await fetchData(BASE_URL);
+        setProducts(data);
+
+        const uniqueCategories = [...new Set(data.map((product) => product.category))];
+        setCategories(uniqueCategories);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataAndCategories();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="allCategories">
+      <h2>Categories List</h2>
+      <ProductList products={products} />
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AllProductCategories;
 
   return (
     <div className="allCategories">
