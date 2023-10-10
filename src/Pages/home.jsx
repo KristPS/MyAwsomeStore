39bda4a2454992330783a6ba26f622a2304d1237
KristPS <126523107+KristPS@users.noreply.github.com>
2023-10-09 22:04:30 -0400

    Update home.jsx

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,32 +1,15 @@
-// Home.jsx
-import React from "react";
-import { Link } from "react-router-dom";
+//  Home.jsx
+// import React from "react";
+// import "../App.css";
 
-const Home = () => {
-  return (
-    <div className="home-page">
-      <h1>Welcome to My Awesome Store</h1>
-      <p>Explore our Products:</p>
-      <ul>
-        <li>
-          <Link to="/allProducts">All Products</Link>
-        </li>
-        <li>
-          <Link to="/allCategories">All Categories</Link>
-        </li>
-        <li>
-          <Link to="/specificCategories">Specific Categories</Link>
-        </li>
-        <li>
-          <Link to="/cartById">Cart by ID</Link>
-        </li>
-        <li>
-          <Link to="/createNewCart">Create New Cart</Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
+// const Home = () => {
+//  return (
+//      <div className="home-page">
+      
+//    </div>  
+//  );
+// }
 
-export default Home;
+// export default Home;
+
 
