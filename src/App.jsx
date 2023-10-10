3e56a72a32e77bb1f8911b417f8d99e73c6ac5ce
KristPS <126523107+KristPS@users.noreply.github.com>
2023-10-10 04:16:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,45 @@
-// App.jsx
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Cart from "./Pages/Cart/getAllCarts.jsx";
 import Products from "./Pages/Products/allProducts.jsx";
-import Signup from "./Pages/signup.jsx";
 import Login from "./Pages/login.jsx";
 import Logout from "./Pages/logout.jsx";
-import NavBar from "./Components/layout.jsx";
-
-
-function Home()  {
-  return (
-<div>
-        <h1>Welcome to the Home Page</h1>
-      </div>
-  )
-}
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("token") || "");
+  
   return (
-    <div className="home">
-      <div>Welcome to the Home Page</div>
-      <NavBar token={token} setToken={setToken} />
-      <Routes>
+   <Router>
+    <div> 
+      <nav>
+        <ul> 
+          <li>
+        <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/allProducts">Products</Link>
+            </li>
+            <li>
+              <Link to="/getAllCarts">Cart</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+              </li>
+        </ul>
+      </nav>
+    
+         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/allProducts" element={<Products />} />
         <Route path="/getAllCarts" element={<Cart />} />
         <Route path="/logout" element={<Logout />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
-        <Route path="/signup" element={<Signup setToken={setToken} />} />
-        
+               
       </Routes>
     </div>
+    </Router>
   );
 }
 
