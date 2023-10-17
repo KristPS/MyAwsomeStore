9393ea825d385b899963438ff1646913d1b1a5c0
abischolz <abi.scholz@gmail.com>
2023-10-17 16:55:30 -0400

    Notes from 10/17 mtg, roadmap for next steps

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -2,13 +2,30 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 
 const BASE_URL = 'https://fakestoreapi.com/auth/login';
+/* To access the props that you passed in, you can do EITHER 
+______________________________________
+export default function Login(props) {
+...
+  // to use what's been passed in, access the props object  
+  props.setToken(...)
+}
+__________________________
+OR destructure the props (you are importing the keys of the props object one by one)
 
+export default function Login({ setToken, user, ...props}) { 
+  // to use, you can simple invoke the function 
+  setToken(...)
+}
+
+*/
 export default function Login() {
+  // use username and password to filter the users array and select the correct user https://fakestoreapi.com/docs#u-all
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate(); 
+  // this is duplicating your state - let's get rid of this 
   const [token, setToken] = useState("");
 
   const handleSubmit = async (e) => {
@@ -29,6 +46,8 @@ export default function Login() {
       if (response.ok) {
         const data = await response.json();
         setToken(data.token);
+        // once you have an OK response, you can set user here. 
+        // since setUser is passed into this component and exists in App.jsx, once you set it here it will be accessible to other components that share this state. 
         localStorage.setItem("token", data.token);
         navigate("/cart");
         console.log("Login successful");
