f035eaa90b81b9a24c296a6eee9494fffb9b2426
KristPS <126523107+KristPS@users.noreply.github.com>
2023-10-26 14:12:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Cart from "./Pages/Cart/getAllCarts";
-import Products from "./Pages/Products/allProducts";
-import Login from "./Pages/login";
-import Logout from "./Pages/logout";
-import Home from "./Pages/home";
-import Checkout from "./Pages/checkout";
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+import ProductList from "./Pages/Files/productList";
+import ProductDetail from "./Pages/Files/productDetails";
+import Cart from "./Pages/Files/cart";
+import Checkout from "./Pages/Files/checkout";
+
+import Login from "./Pages/Files/login";
+import Logout from "./Pages/Files/logout";
 
 function App() {
-  // This is the entry point of your app - anything that you want to use across different components (like token, user, cart)
-  // can be fetched here and set in state 
-  const [token, setToken] = useState("");
-  // users, setUsers 
+  
+   
   return (
-    
+    <Router>
       <div>
         <nav>
           <ul>
@@ -21,13 +21,10 @@ function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/products">Products</Link>
-            </li>
-            <li>
-              <Link to="/carts">Cart</Link>
-            </li>
-            <li>
               <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
             </li>
             <li>
               <Link to="/logout">Logout</Link>
@@ -35,19 +32,18 @@ function App() {
           </ul>
         </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          {/* Once a user has logged in, you can pass that user's info into the cart component to get user's cart.  */}
-          <Route path="/carts" element={<Cart />} />
+        <Routes>  {/* Wrap all Route components inside Routes */}
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
-          {/* You will also want to pass in setUser to your Logout component - when you log out, you can set user to null to clear all user info */}
+         
+          <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
-          {/* once you fetch all users, you can pass them into the Login component (example: users={users} setUser={setUser}) */}
-          <Route path="/login" element={<Login setToken={setToken} />} />
+        
         </Routes>
       </div>
-    
+    </Router>
   );
 }
 
