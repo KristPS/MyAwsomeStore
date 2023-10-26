fc7289726b6daf21d94f4a676990450710ac4ee2
KristPS <126523107+KristPS@users.noreply.github.com>
2023-10-26 14:37:56 -0400

    Update login.jsx

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,94 +1,36 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom"; 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigation instead of useHistory
 
-const BASE_URL = 'https://fakestoreapi.com/auth/login';
-/* To access the props that you passed in, you can do EITHER 
-______________________________________
-export default function Login(props) {
-...
-  // to use what's been passed in, access the props object  
-  props.setToken(...)
-}
-__________________________
-OR destructure the props (you are importing the keys of the props object one by one)
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Use useNavigation hook
 
-export default function Login({ setToken, user, ...props}) { 
-  // to use, you can simple invoke the function 
-  setToken(...)
-}
-
-*/
-export default function Login() {
-  // use username and password to filter the users array and select the correct user https://fakestoreapi.com/docs#u-all
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate(); 
-  // this is duplicating your state - let's get rid of this 
-  const [token, setToken] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setError(null);
-    setLoading(true);
-
-    try {
-      const response = await fetch(BASE_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-          
-      if (response.ok) {
-        const data = await response.json();
-        setToken(data.token);
-        // once you have an OK response, you can set user here. 
-        // since setUser is passed into this component and exists in App.jsx, once you set it here it will be accessible to other components that share this state. 
-        localStorage.setItem("token", data.token);
-        navigate("/cart");
-        console.log("Login successful");
-      } else {
-        const errorData = await response.json();
-        setError(errorData.message || "Login failed");
-        console.error("Login failed:", errorData);
-      }
-    } catch (error) {
-      setError("An error occurred. Please try again.");
-      console.error("An error occurred", error);
-    } finally {
-      setLoading(false);
+  const handleLogin = () => {
+    if (username && password) {
+      localStorage.setItem('user', JSON.stringify({ username, password }));
+      navigate('/'); // Use navigate instead of history.push
+    } else {
+      alert('Please enter valid credentials!');
     }
   };
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={e => e.preventDefault()}>
         <label>
           Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
         </label>
         <label>
           Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
         </label>
-        <button type="submit" disabled={loading}>
-          {loading ? "Logging in..." : "Login"}
-        </button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button type="button" onClick={handleLogin}>Login</button>
       </form>
     </div>
   );
 }
+
+export default Login;
